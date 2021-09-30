012f961c0c50a75067b64840fafcfcf49abc2130
Maurice Parker <mo@vincode.io>
2021-09-30 10:29:42 -0500

    Initial Commit

---
+++ Shared/RinggoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  RinggoldApp.swift
+//  Shared
+//
+//  Created by Maurice Parker on 9/30/21.
+//
+
+import SwiftUI
+
+@main
+struct RinggoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
