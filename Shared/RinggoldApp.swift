3910be500e9c4ebbb5cc70b538d85254c1dd6b71
Maurice Parker <mo@vincode.io>
2021-09-30 14:43:18 -0500

    Stub out url handler

--- Shared/RinggoldApp.swift
+++ Shared/RinggoldApp.swift
@@ -9,16 +9,35 @@ import SwiftUI
 
 @main
 struct RinggoldApp: App {
+	
+	let frameWidth: CGFloat = 400
+	let frameHeight: CGFloat = 400
+
     var body: some Scene {
 		#if os(macOS)
         WindowGroup {
             ContentView()
-				.frame(minWidth: 400, idealWidth: 400, maxWidth: 400, minHeight: 400, idealHeight: 400, maxHeight: 400)
+				.frame(minWidth: frameWidth, idealWidth: frameWidth, maxWidth: frameWidth, minHeight: frameHeight, idealHeight: frameHeight, maxHeight: frameHeight)
+				.onOpenURL { url in
+					handle(url: url)
+				}
         }
 		#else
 		WindowGroup {
 			ContentView()
+				.onOpenURL { url in
+					handle(url: url)
+				}
 		}
 		#endif
     }
+	
+}
+
+private extension RinggoldApp {
+	
+	func handle(url: URL) {
+		
+	}
+	
 }
