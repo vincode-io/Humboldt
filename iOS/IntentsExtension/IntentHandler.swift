c9d350039fac2e124bff76b4623aba4029cb7526
Maurice Parker <mo@vincode.io>
2021-10-01 17:16:13 -0500

    Added Intents targets

---
+++ iOS/IntentsExtension/IntentHandler.swift
@@ -0,0 +1,19 @@
+//
+//  IntentHandler.swift
+//
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+	override func handler(for intent: INIntent) -> Any {
+		switch intent {
+//		case is AddWebFeedIntent:
+//			return AddWebFeedIntentHandler()
+		default:
+			fatalError("Unhandled intent type: \(intent)")
+		}
+	}
+    
+}
