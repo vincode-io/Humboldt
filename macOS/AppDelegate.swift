ca87d7fdd4b510c893a693d52a69c376bb38a41e
Maurice Parker <mo@vincode.io>
2021-09-30 19:09:10 -0500

    Remove as many menu items as possible that aren't relevant

--- macOS/AppDelegate.swift
+++ macOS/AppDelegate.swift
@@ -9,6 +9,13 @@ import AppKit
 
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+	func applicationDidFinishLaunching(_ notification: Notification) {
+		let mainMenu = NSApplication.shared.mainMenu!
+		for i in 1..<mainMenu.numberOfItems {
+			mainMenu.removeItem(at: i)
+		}
+	}
+	
 	func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
 		return true
 	}
